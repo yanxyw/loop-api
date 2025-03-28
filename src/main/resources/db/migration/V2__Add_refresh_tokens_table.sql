5c4b1acabd97b592f142fd6e17fb185194994d69
Evelyn <96553159+evelyn-2022@users.noreply.github.com>
2025-03-28 22:08:36 +0000

    Use flyway for db migration

---
+++ src/main/resources/db/migration/V2__Add_refresh_tokens_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE refresh_tokens
+(
+    id          BIGSERIAL PRIMARY KEY,
+    token       VARCHAR(255) NOT NULL UNIQUE,
+    expiry_date TIMESTAMP    NOT NULL,
+    user_id     BIGINT,
+    CONSTRAINT fk_refresh_tokens_user FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE INDEX idx_refresh_tokens_user ON refresh_tokens (user_id);
